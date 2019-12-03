2582495e00c4d080637f82b507a510ce30c8e161
Kadir <nametext>
2019-12-03 21:52:02 +0300

    Initial Commit

---
+++ munzueYemekListesi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  munzueYemekListesi
+//
+//  Created by Kadir Aktas on 3.12.2019.
+//  Copyright © 2019 Kadir Aktas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ munzueYemekListesiTests/munzueYemekListesiTests.swift
@@ -0,0 +1,34 @@
+//
+//  munzueYemekListesiTests.swift
+//  munzueYemekListesiTests
+//
+//  Created by Kadir Aktas on 3.12.2019.
+//  Copyright © 2019 Kadir Aktas. All rights reserved.
+//
+
+import XCTest
+@testable import munzueYemekListesi
+
+class munzueYemekListesiTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
